f0b56797891dea7e6e2ba63802d56eae585e0d13
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-04-03 12:10:58 +0530

    fixed crashing of the add() in dynamic array

--- Data Structures/dynamicArray.cpp
+++ Data Structures/dynamicArray.cpp
@@ -2,20 +2,21 @@
 #include<cstdlib>
 #include<stdexcept>
 using namespace std;
-template <class T> class dynamicArray{
+template <typename T> class dynamicArray{
 private:
-	T arr[];
+	
 	unsigned int len = 0;	//length user thinks
 	unsigned int capacity = 0;	//actual array size
+	T arr[];
 
 public:
 	/*constructors*/
 	dynamicArray(){
-		this->capacity = 16;
+		capacity = 2;
 	}
 	dynamicArray(int capacity){
 		this->capacity = capacity;
-		arr = (T[]) new dynamicArray[capacity];
+		T arr[this->capacity];
 	}
 
 	int size();
@@ -23,16 +24,24 @@ public:
 	T get(int index);
 	void set(int index,T elem);
 	void clear();
-	void add(T elem);
+	T *add(T elem);
 	T removeAt(int rm_index);
 	bool remove(dynamicArray obj);
 
 	int indexOf(dynamicArray obj);
-	bool containc(dynamicArray obj);
+	bool contains(dynamicArray obj);
 };
 
+/*main function*/
 int main(int argc, char const *argv[])
 {
+	dynamicArray<int> arr;
+	arr.add(1);
+	for (int i = 0; i < 100; ++i)
+	{
+		arr.add(i);
+	}
+	cout << arr.get(0);
 
 	system("pause");
 	return 0;
@@ -44,7 +53,7 @@ int dynamicArray<T>::size(){
 }
 template <class T>
 bool dynamicArray<T>::isEmpty(){
-	return size == 0;
+	return len == 0;
 }
 template <class T>
 T dynamicArray<T>::get(int index){
@@ -63,37 +72,88 @@ void dynamicArray<T>::clear(){
 	}
 	len = 0;
 }
-template <class T>
 
-void dynamicArray<T>::add(T elem){
-	if (len + 1 >= capacity)
+
+template <class T>
+T *dynamicArray<T>::add(T elem){
+	if (len + 1 > capacity)
 	{
 		if (capacity == 0)
 		{
-			capacity = 1;
+			capacity += 1;
 		}
 		else{
 			capacity *= 2;
 		}
 	}
 
-
-	T new_arr[] = (T[]) new dynamicArray[capacity];
-
+	cout << capacity << endl;
+	T *new_arr;
+	new_arr = new T[capacity];
 	for (int i = 0; i < len; ++i)
 	{
-		arr[new_arr];
+		/* copy the value from array to new array */
+		new_arr[i] = arr[i];
 	}
+	/*adding the last value*/
+	new_arr[len++] = elem;
+	
+	return new_arr;
+ }
 
-	arr[len++] = elem;
-}
+
 template<class T>
 T dynamicArray<T>::removeAt(int rm_index){
 	if(rm_index >= len || rm_index < 0) throw new std::out_of_range("Index out of bound!");
 	T data = arr[rm_index];
 
-	T new_arr[] = (T[]) new dynamicArray[len-1];
+	T new_arr[len-1];
 
+	for (int i = 0; i < len; ++i)
+	{
+		if (rm_index == i)
+		{
+			continue;
+		}
+		else{
+			new_arr[i] = arr[i];
+		}
+	}
+	arr = new_arr;
+	capacity = --len;
+	return data;
 
-
+}
+template <class T>
+bool dynamicArray<T>::remove(dynamicArray obj){
+	int index = indexOf(obj);
+	if (index == -1)
+	{
+		return false;
+	}
+	removeAt(index);
+	return true;
+}
+template<class T>
+int dynamicArray<T>::indexOf(dynamicArray obj){
+	for (int i = 0; i < len; ++i)
+	{
+		if (obj == NULL)
+		{
+			if (arr[i] == NULL)
+			{
+				return i;
+			}
+			else{
+				if(obj.equals(arr[i])){
+					return i;
+				}
+			}
+		}
+	}
+	return -1;
+}
+template <class T>
+bool dynamicArray<T>::contains(dynamicArray obj){
+	return indexOf(obj) != -1;
 }
