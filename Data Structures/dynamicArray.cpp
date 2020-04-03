e31b1e179131ef3a7b4cdc54860f24fa8fc7b312
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-04-03 16:09:41 +0530

    fixed removeAt and remove

--- Data Structures/dynamicArray.cpp
+++ Data Structures/dynamicArray.cpp
@@ -22,23 +22,36 @@ public:
 	void clear();
 	void add(T elem);
 	T removeAt(int rm_index);
-	bool remove(dynamicArray obj);
+	bool remove();
 
-	int indexOf(dynamicArray obj);
-	bool contains(dynamicArray obj);
+	int indexOf(T elem,int start = 0);
+	
 };
 
 /*main function*/
 int main(int argc, char const *argv[])
 {
+	/*dynamicArray<int> arr;
+	for (int i = 0; i < 10; ++i)
+	{
+		arr.add(i);
+	}
+	arr.removeAt(5);
+	cout << arr.get(5) << endl;
+	cout << arr.size();
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << arr.get(i) << endl;
+	}
+	arr.clear();
+	cout << "GEt" << endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << arr.get(i) << endl;
+	}*/
 	dynamicArray<int> arr;
-	//arr.add(1);
-	arr.add(5);
-	arr.add(45);
-	cout << arr.get(0) << endl;
-	cout << arr.get(1) << endl;
-	
 
+	cout << arr.isEmpty();
 	system("pause");
 	return 0;
 }
@@ -53,16 +66,20 @@ bool dynamicArray<T>::isEmpty(){
 }
 template <class T>
 T dynamicArray<T>::get(int index){
+	/*get element according to index given*/
 	return arr[index];
 }
 template <class T>
 void dynamicArray<T>::set(int index,T elem){
+	/*set element to particular index in the array, it replaces the previous value with the given one
+	*/
 	arr[index] = elem;
 }
 
 template <class T>
 void dynamicArray<T>::clear(){
-	for (int i = 0; i < len; ++i)
+	/*make the array NULL*/
+	for (int i = 0; i < len+1; ++i)
 	{
 		arr[i] = '\0';
 	}
@@ -72,7 +89,7 @@ void dynamicArray<T>::clear(){
 
 template <class T>
 void dynamicArray<T>::add(T elem){
-	
+	/*if the capacity is less tnan increase it by x2 */
 	if (len + 1 > capacity)
 	{
 		if (capacity == 0)
@@ -102,56 +119,52 @@ void dynamicArray<T>::add(T elem){
 
 template<class T>
 T dynamicArray<T>::removeAt(int rm_index){
-	if(rm_index >= len || rm_index < 0) throw new std::out_of_range("Index out of bound!");
-	T data = arr[rm_index];
+	/*
+	given index, delete element at the given position
 
-	T new_arr[len-1];
+	*/
+	//if index is greater than length or negative
+	if(rm_index >= len || rm_index < 0) 
+		//throws indexOutOfBoundException
+		throw new std::out_of_range("Index out of bound!");
+	T data = arr[rm_index];				/*value of array at this index*/
+	/*array which is 1 value smaller than the actual array*/
+	/*creating new array with 1 element less space for element*/
+	T *new_arr = new T[len-1];
 
-	for (int i = 0; i < len; ++i)
-	{
-		if (rm_index == i)
-		{
-			continue;
-		}
-		else{
-			new_arr[i] = arr[i];
-		}
+	int j = 0;
+	for(int i = 0; i < len; i++){
+  	/*if i matches the removed index*/
+  	if(i == rm_index){
+    	continue;
+  	}
+  		new_arr[j] = arr[i];
+  		j++;
 	}
-	arr = new_arr;
+	arr = new_arr;	/*copy*/
 	capacity = --len;
 	return data;
 
 }
 template <class T>
-bool dynamicArray<T>::remove(dynamicArray obj){
-	int index = indexOf(obj);
-	if (index == -1)
-	{
-		return false;
-	}
-	removeAt(index);
+bool dynamicArray<T>::remove(){
+	/*removes the last element*/
+	arr.removeAt(len);
 	return true;
 }
 template<class T>
-int dynamicArray<T>::indexOf(dynamicArray obj){
-	for (int i = 0; i < len; ++i)
+int dynamicArray<T>::indexOf(T elem,int start){
+	/*searching for element, start provides the start position from where
+	we have to start findding the element in the array*/
+	for (int i = start; i < len; ++i)
 	{
-		if (obj == NULL)
+		/*if given element matches in given array*/
+		if (arr[i] == elem)
 		{
-			if (arr[i] == NULL)
-			{
-				return i;
-			}
-			else{
-				if(obj.equals(arr[i])){
-					return i;
-				}
-			}
+			/*retruns the position*/
+			return i;
 		}
 	}
+	/*if element was not present, it will return -1*/
 	return -1;
-}
-template <class T>
-bool dynamicArray<T>::contains(dynamicArray obj){
-	return indexOf(obj) != -1;
 }
