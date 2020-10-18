5a3d433a2aab64ddc5876125c43d64d5a15cfc67
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-10-18 09:48:28 +0530

    added insert_at_position method

--- Data Structures/python/doubly_linked_list.py
+++ Data Structures/python/doubly_linked_list.py
@@ -7,10 +7,12 @@ class Node:
 class Doubly_linked_list:
     head = None
     tail = None
+    
     def __init__(self,data):
         new_node = Node(data)
         self.head = new_node
         self.tail = new_node
+    
     def insert_at_front(self,data):
         new_node = Node(data)
         if self.head is None:
@@ -34,6 +36,27 @@ class Doubly_linked_list:
         self.tail.nextNode = new_node
         self.tail = new_node
 
+    def insert_at_position(self,position,data):
+        if position == 0:
+            return self.insert_at_front(data)
+        if self.head is None:
+            return "List is already empty"
+        new_node = Node(data)
+        current_node = self.head
+        for i in range(position):
+            if current_node == self.tail:
+                return "List does not have index!"
+            current_node = current_node.nextNode
+        
+        new_node.nextNode = current_node.nextNode
+        new_node.prevNode = current_node
+
+        current_node.nextNode = new_node
+
+
+          
+
+
     def delete_at_front(self):
         temp = self.head
         self.head.nextNode.prevNode = None
@@ -45,6 +68,21 @@ class Doubly_linked_list:
         self.tail.prevNode.nextNode = None
         self.tail = self.tail.prevNode
         del temp
+
+    def delete_at_position(self,position):
+        if self.head is None:
+            return "List is already empty!"
+        current = self.head
+        
+        for i in range(position):
+
+            current = current.nextNode
+        
+        current.prevNode.nextNode = current.nextNode
+        current.nextNode.prevNode = current.prevNode
+
+        del current
+
 
     def print_forward(self):
         temp = self.head
@@ -61,5 +99,6 @@ if __name__ == "__main__":
     dll.delete_at_front()
     dll.delete_at_front()
     dll.insert_at_front(250)
-    dll.delete_at_last()
+    dll.insert_at_position(3,3)
+    dll.delete_at_position(3)
     dll.print_forward()
