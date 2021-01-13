e5d2a159e0c8e9797d7197edd9a0564e212be30d
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2021-01-13 12:53:40 +0530

    added hashtable code data structure

--- Data Structures/cpp/hashTable.cpp
+++ Data Structures/cpp/hashTable.cpp
@@ -1,82 +1,88 @@
 #include<iostream>
-#ifndef SIZE
-#define SIZE 20
+#include<string>
+#ifndef TABLE_SIZE
+#define TABLE_SIZE 3333
 #endif
 
-struct DataItem{
-	int data,key;
-};
-struct DataItem* hashArray[SIZE];
-struct DataItem* dummyItem;
-struct DataItem* item;
-void display();
-
-int hashCode(int key){
-	return key % SIZE;
-}
-
-struct DataItem *search(int key);
-void insert(int key,int data);
-struct DataItem *delete_item(struct DataItem *item);
-
-int main(int argc, char const *argv[])
-{
+template<typename T>
+class HashTable{
+	private:
+	 T *table = new T[TABLE_SIZE];
+	 int numItems = 0;
 	
+	public:
+	int hashStringToInt(std::string s,int tableSize);
+
+	void resize();
+  	void setItem(std::string key,T value);
+	void getItem(std::string key);
+};
+
+int main(int argc,char const **argv){
+	
+	HashTable<std::string> mytable;
+	mytable.setItem("sajid","great");
+	
+	
+
+	mytable.getItem("sajid");
+	
+
 	return 0;
 }
 
-struct DataItem *search(int key){
-	int hashIndex = hashCode(key);
 
-	while(hashArray[hashIndex] != NULL){
-
-		if(hashArray[hashIndex]->key == key){
-			return hashArray[hashIndex];
-		}
-
-		++hashIndex;
-
-		hashIndex %= SIZE;
+template<class T>
+int HashTable<T>::hashStringToInt(std::string s,int tableSize){
+	int hash = 17;
+	for(int i = 0;i < s.size();i++){
+		hash = (13 * hash * char(s[i])) % tableSize;
 	}
-	return NULL;
+
+	return hash;
 }
-void insert(int key,int data){
-	struct DataItem *item = new DataItem;
-	item->data = data;
-	item->key = key;
 
-	int hashIndex = hashCode(key);
+template<class T>
+void HashTable<T>::setItem(std::string key,T value){
+	this->numItems++;
 
-	while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1){
-		++hashIndex;
-		hashIndex %= SIZE;
-	}
-	hashArray[hashIndex] = item;
-
-}
-struct DataItem *delete_item(struct DataItem *item){
-	int key = item->key;
-
-	int hashIndex = hashCode(key);
-
-
-	while(hashArray[hashIndex] != NULL){
-		if(hashArray[hashIndex]->key == key){
-			struct DataItem *temp = hashArray[hashIndex];
-
-			hashArray[hashIndex] = dummyItem;
-			return temp;
-		}
-		++hashIndex;
-		hashIndex %= SIZE;
-	}	
-	return NULL;
-}
-void display(){
 	
-	for(int i =0;i < SIZE;i++){
-		if(hashArray[i] != NULL){
-			std::cout << "("<<hashArray[i]->key<< "," <<hashArray[i] -> data<<")";
-		}
+
+	int arrayLength = (sizeof(this->table) / sizeof(*this->table));
+	const int loadFactor = this->numItems / arrayLength;
+
+	if(loadFactor > 0.8){
+		/*resize the array*/
+		resize();
 	}
+	arrayLength = (sizeof(this->table) / sizeof(*this->table));
+	const int idx = hashStringToInt(key,arrayLength);
+	this->table[idx] = value;
+}
+
+template<class T>
+void HashTable<T>::getItem(std::string key){
+	this->numItems++;
+	int arrayLength = (sizeof(this->table)/sizeof(*this->table));
+	int idx = hashStringToInt(key,arrayLength);
+
+
+	try{
+		std::cout << this->table[idx]; 
+	}
+	catch(const std::out_of_range& oor){
+		std::cout << "Not found";
+	}
+}
+
+template<class T>
+void HashTable<T>::resize(){
+	
+	int arrayLength = (sizeof(this->table)/sizeof(*this->table));
+	T* newTable = new T[arrayLength*2];
+
+	std::copy(this->table, this->table + std::min(arrayLength, arrayLength*2), newTable);
+	
+	delete[] this->table;
+	this->table = newTable;
 }
