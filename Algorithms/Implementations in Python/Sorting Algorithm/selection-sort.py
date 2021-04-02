1cd1eb63e355efa1353b1be7d2e3fabd29fa4a39
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2021-04-02 15:02:11 +0530

    code explanations

--- Algorithms/Implementations in Python/Sorting Algorithm/selection-sort.py
+++ Algorithms/Implementations in Python/Sorting Algorithm/selection-sort.py
@@ -1,14 +1,13 @@
-import os
 
 def selection_sort(arr):
 	if len(arr)<2:
 		return arr
-	for i in range(len(arr)):
-		min_index = i
-		for j in range(i+1,len(arr)):
+	for i in range(len(arr)): #traversing array and holding one element at a time
+		min_index = i #assume first element to be the smallest
+		for j in range(i+1,len(arr)):#now traverse from the smallest assumption to the end
 			if arr[min_index] > arr[j]:
-				min_index = j
-		arr[i],arr[min_index] = arr[min_index],arr[i]
+				#if minimum assumption was lower then current element
+				min_index = j #change min to current
+		arr[i],arr[min_index] = arr[min_index],arr[i] #now swap the smallest with the current one
 		return arr
 print(selection_sort([9,8,0,-9])) #outputs -> [-9,8,0,9]		
-os.system("pause")
